3cc84f47c0d263bc6f03ab326bed0e2856156753
ronia-jakim <weronikajakimowicz2@gmail.com>
2023-03-15 08:15:21 +0100

    nie-wiem

---
+++ pracB/zadB.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+// można max 2 razy pod rząd w górę
+// graf skierowany? topo sortem znajduję możliwe ścieżki startujące z każdej możliwej na pierwszym rzędzie i szukam najdłuższej ścieżki?
+// złożoność to chyba jakieś n * log n + n?
+
+#define ll long long int
+
+using namespace std;
+
+ll w, k;
+
+vector<ll> PLANSZA [10001];
+
+std::ios::sync_with_stdio(false)
+
+
+int main () {
+    cin >> w >> k;
+    return 0;
+}
