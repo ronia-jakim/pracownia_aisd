5f55d8c0acac7b7f29a68bc001a1f59f9799062e
ronia-jakim <weronikajakimowicz2@gmail.com>
2023-03-28 12:16:48 +0200

    dupa

--- pracB/tests/my_test.sh
+++ pracB/tests/my_test.sh
@@ -1,14 +1,25 @@
+T=0
 S=0
 M=0
 B=0
 
-g++ ../zadA.cpp -o zadA
+g++ ../zadB.cpp -o zadB
+
+for i in {1..1000}
+do
+    echo `./zadB < ./tiny/in/$i.in` > dupa
+    if cmp ./dupa tiny/out/$i.out; then
+        ((T++))
+    else
+        echo "DUPA $i"
+    fi
+done
 
 for i in {1..100}
 do
 	#echo Testing small: $i
 	#/usr/bin/time -f "Running time: %E" ./prog < small/in/$i.in > TT
-	echo `./zadA < ./small/in/$i.in` > dupa
+	echo `./zadB < ./small/in/$i.in` > dupa
 	if cmp ./dupa small/out/$i.out; then
 		((S++))
 		#echo "OK"
@@ -21,7 +32,7 @@ for i in {1..100}
 do
 	#echo Testing medium: $i
 	#/usr/bin/time -f "Running time: %E" ./prog < small/in/$i.in > TT
-	echo `./zadA < ./med/in/$i.in` > dupa
+	echo `./zadB < ./med/in/$i.in` > dupa
 	if cmp ./dupa med/out/$i.out; then
 		((M++))
 		#echo "OK"
@@ -30,20 +41,21 @@ do
 	fi
 done
 
-for i in {1..10}
-do
+#for i in {1..10}
+#do
 	#echo Testing fat: $i
 	#/usr/bin/time -f "Running time: %E" ./prog < small/in/$i.in > TT
-	echo `./zadA < ./fat/in/$i.in` > dupa
-	if cmp ./dupa fat/out/$i.out; then
-		((B++))
+#	echo `./zadB < ./fat/in/$i.in` > dupa
+#	if cmp ./dupa fat/out/$i.out; then
+#		((B++))
 		#echo "OK"
-	else
-		echo "DUPA $i"
-	fi
-done
+#	else
+#		echo "DUPA $i"
+#	fi
+#done
 
 
+echo "tiny tests: $T/1000"
 echo "small tests: $S/100"
 echo "medium tests: $M/100"
 echo "fat tests: $B/10"
--- pracB/zadB.cpp
+++ pracB/zadB.cpp
@@ -13,12 +13,12 @@ int w, k;
 
 int potega[10];
 
-ll DUPEK [18][10000][2];
-
+ll DUPEK [18][10000];
+int POT[18][10000];
 
 int main () {
-    // std::ios::sync_with_stdio(false);
-    // cin.tie(NULL);
+    std::ios::sync_with_stdio(false);
+    cin.tie(NULL);
     //
     potega[0] = 1;
     for (int i = 1; i < 10; i++) {
@@ -28,31 +28,89 @@ int main () {
     cin >> w >> k;
 
     for (int i = 0; i < w; i++) {
+        if (k == 2 && i % 2 == 1) continue;
         for (int j = 0; j < k; j++) {
             int pom;
             char g;
             cin >> g;
             pom = int(g) - 48;
 
-            DUPEK[5][j] += potega[pom];
-
-            if (j > 0) DUPEK[7][j-1][0] = max(DUPEK[7][j-1][0], DUPEK[5][j]);
-            if (j > 1) DUPEK[6][j-2][0] = max(DUPEK[6][j-2][0], DUPEK[5][j]);
-            if (j < k-1) DUPEK[7][j+1] = max(DUPEK[7][j+1], DUPEK[5][j]);
-            if (j < k-2) DUPEK[6][j+2] = max(DUPEK[6][j+2], DUPEK[5][j]);
-
-            if (j < k-2) DUPEK[5][j+2] = max(DUPEK[5][j+2], DUPEK[5][j] + DUPEK[3][j+1]);
-            if (j < k-5) DUPEK[5][j+4] = max(DUPEK[5][j] + DUPEK[4][j+2], DUPEK[5][j+4]);
-            if (j < k-4) DUPEK[6][j+3] = max(DUPEK[5][j] + DUPEK[4][j+2], DUPEK[6][j+3]);
-            if (j < k-3) DUPEK[6][j+1] = max(DUPEK[5][j] + DUPEK[4][j+2], DUPEK[6][j+1]);
-            if (j > 1) DUPEK[6][j-1] = max(DUPEK[5][j] + DUPEK[4][j-2], DUPEK[6][j-1]);
-            if (j > 2) DUPEK[6][j-3] = max(DUPEK[5][j] + DUPEK[4][j-2], DUPEK[6][j-2]);
-
+            POT[5][j] = potega[pom];
         }
         for (int j = 0; j < k; j++) {
-            for (int l = 0; l < 8; l++) DUPEK[l][j] = DUPEK[l+1][j];
+
+            int krok[2][2] = {{2, 1}, {2, -1}};
+            
+            DUPEK[5][j] = 0;
+
+            for (int m = 0; m < 2; m++) {
+                if (0 <= j+krok[m][1] && j+krok[m][1] < k) {
+                    int x = 5-krok[m][0];
+                    int y = j+krok[m][1];
+                    for (int l = 0; l < 2; l++) {
+                        if (0 <= y+krok[l][1] && y+krok[l][1] < k) {
+                            int a = 5-krok[m][0]-krok[l][0];
+                            int b = j+krok[m][1]+krok[l][1];
+ 
+                            DUPEK[5][j] = max(DUPEK[5][j], DUPEK[a][b] + POT[x][y]);
+                        }
+                    }
+                }
+            }                    
+            
+            if (i % 2 == 1) {
+                if (j + 2 >= k || j-2 < 0) DUPEK[5][j] = 0;
+            }
+
+            if (i >= 3){
+                if (j+1 < k && j-1 >= 0) {
+                    DUPEK[5][j] = max(DUPEK[5][j], DUPEK[4][j+1] + POT[3][j-1]);
+                }
+                if (j-3 >= 0) {
+                    DUPEK[5][j] = max(DUPEK[5][j], DUPEK[4][j-3] + POT[3][j-1]);
+                }
+
+                if (j-1 >= 0 && j+1 < k) {
+                    DUPEK[5][j] = max(DUPEK[5][j], DUPEK[4][j-1] + POT[3][j+1]);
+                }
+                if (j+3 < k) {
+                    DUPEK[5][j] = max(DUPEK[5][j], DUPEK[4][j+3] + POT[3][j+1]);
+                }
+            }
+
+            if (i >= 1) {
+                if (j+2 < k) {
+                    DUPEK[4][j+2] = max(DUPEK[4][j+2], DUPEK[5][j]+POT[4][j+2]);
+                }
+                if (j-2 >= 0) {
+                    DUPEK[4][j-2] = max(DUPEK[4][j-2], DUPEK[5][j]+POT[4][j-2]);
+           //         if (j+2 < k) DUPEK[6][j+1] = max(DUPEK[4][j], DUPEK[4][j+2]);
+           //         DUPEK[6][j-1] = max(DUPEK[4][j-2], DUPEK[4][j]);
+                }
+            }
+
+            DUPEK[5][j] += POT[5][j];
+
+        }
+
+//        for (int j = 0; j < 6; j++) {
+//            for (int l = 0; l < k; l++) {
+//                cout << DUPEK[j][l] << " ";
+//            }
+//            cout << "\n";
+//        }
+
+//        cout << "= = = = = = = =\n";
+
+
+        for (int j = 0; j < k; j++) {
+            for (int l = 0; l < 8; l++) {
+                DUPEK[l][j] = DUPEK[l+1][j];
+                POT[l][j] = POT[l+1][j];
+            }
         }
     }
+
 
     ll ret = 0;
     for (int i = 0; i < k; i++) {
